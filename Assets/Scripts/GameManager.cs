370c0f62aedd74c7744f362d678a5afce567caf0
Diego Benites <banitessebastian2decundaria@gmail.com>
2026-05-13 12:13:55 -0500

    avance

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Sirenix.OdinInspector;
+
+public class GameManager : MonoBehaviour
+{
+    /*[Header("Players")]
+    public List<PlayerData> players = new();
+    public List<Ship> ships;
+    [Header("UI")]
+    public TMP_Text currentPlayerText;
+
+    [ReadOnly]
+    public int currentTurn;
+
+    private void Start()
+    {
+        UpdateUI();
+    }
+
+    [Button]
+    public void NextTurn()
+    {
+        currentTurn++;
+
+        if (currentTurn >= players.Count)
+        {
+            currentTurn = 0;
+        }
+            UpdateUI();
+    }
+
+    void UpdateUI()
+    {
+        PlayerData currentPlayer = players[currentTurn];
+
+        currentPlayerText.text =
+            currentPlayer.playerName +
+            " - " +
+            currentPlayer.shipColor;
+    }*/
+}
