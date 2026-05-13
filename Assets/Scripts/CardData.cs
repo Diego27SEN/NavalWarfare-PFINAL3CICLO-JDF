854af40ceaee6e34e3aec5694d902f3d65afa66d
Frances2345 <francesco.moreno@tecsup.edu.pe>
2026-05-13 11:14:42 -0500

    emairun emirun emirun emirun emirun emirun emirun emirun emirun emirun emirun emirun emirun emirun emirun emirun

---
+++ Assets/Scripts/CardData.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class CardData : MonoBehaviour
+{
+    public enum CardType
+    {
+        Common,
+        Rare,
+        Epic,
+        Legendary
+    }
+
+        public CardType Type;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
