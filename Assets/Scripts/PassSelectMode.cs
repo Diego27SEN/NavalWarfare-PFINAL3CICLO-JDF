6e0845e61c6de0ee36694aaf5669323df657a7fa
Frances2345 <francesco.moreno@tecsup.edu.pe>
2026-05-19 11:04:50 -0500

    UI titulo y opciones de Localizacion (Proceso)
    la UI del Menu funciona al abrir el panel options, El Boton Play carga la Escena del Menu

---
+++ Assets/Scripts/PassSelectMode.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PassSelectMode : MonoBehaviour
+{
+
+    public void MenuScene()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
