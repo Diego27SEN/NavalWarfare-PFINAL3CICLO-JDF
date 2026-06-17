5c1cd578a84256875c1b2f4398d5d4ecdaed98ac
Moises <francesco.moreno@tecsup.edu.pe>
2026-06-17 10:24:56 -0500

    Intento de mejorar la Pantalla de Carga

--- Assets/Tactical Naval Warfare/Scripts/Core/UI/SceneLoadManager.cs
+++ Assets/Tactical Naval Warfare/Scripts/Core/UI/SceneLoadManager.cs
@@ -6,16 +6,64 @@ using System.Collections;
 
 public class SceneLoadManager : MonoBehaviour
 {
+    public int counter = 0;
     [SerializeField] private Slider loadbar;
     [SerializeField] private GameObject loadPanel;
 
     public void SceneLoad(int sceneIndex)
     {
         loadPanel.SetActive(true);
-        StartCoroutine(LoadAsync(sceneIndex));
+        StartCoroutine(LoadYourAsyncScene(sceneIndex));
     }
 
-    IEnumerator LoadAsync(int sceneIndex)
+    IEnumerator LoadYourAsyncScene(int sceneIndex)
+    {
+        
+        loadbar.value = 0;
+        Debug.Log("Counter: ");
+
+        while (true)
+        {
+          //  yield return new WaitForSeconds(0.5f);
+            counter++;
+            Debug.Log("Counter: " + counter);
+            float progressTarget = Mathf.Clamp01(counter*0.5f /5f);
+            loadbar.value = progressTarget;
+
+
+            
+            if (loadbar.value >= 1) break;
+            
+            /* SceneManager.LoadScene(sceneIndex);
+             yield return null;*/
+        }
+        yield return new WaitUntil(() => loadbar.value >= 1);
+        SceneManager.LoadScene(sceneIndex);
+        
+        Debug.Log("Counter fin ");
+        yield return null;
+
+        /* AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneIndex);
+
+         asyncLoad.allowSceneActivation = false;
+
+         while (!asyncLoad.isDone)
+         {
+             float progressTarget = Mathf.Clamp01(asyncLoad.progress / 0.9f);
+             loadbar.value = Mathf.MoveTowards(loadbar.value, progressTarget, Time.deltaTime);
+
+             if (loadbar.value >= 1)
+             {
+                 yield return new WaitForSeconds(1f);
+
+                 asyncLoad.allowSceneActivation = true;
+             }
+
+             yield return null;
+         }*/
+
+    }
+    /*IEnumerator LoadAsync(int sceneIndex)
     {
         AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneIndex);
 
@@ -25,11 +73,49 @@ public class SceneLoadManager : MonoBehaviour
             loadbar.value = asyncOperation.progress;
             yield return null;
         }
+    }*/
+
+   /* IEnumerator LoadYourAsyncScene(int sceneIndex)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneIndex);
+
+        asyncLoad.allowSceneActivation = false;
+
+        while (!asyncLoad.isDone)
+        {
+            float progressTarget = Mathf.Clamp01(asyncLoad.progress / 0.9f);
+            loadbar.value = Mathf.MoveTowards(loadbar.value, progressTarget, Time.deltaTime);
+
+            if (loadbar.value >= 1)
+            {
+                yield return new WaitForSeconds(1f);
+
+                asyncLoad.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+
+    */
+    /*
+     private int counter = 0;
+
+    void Start()
+    {
+        // Start the coroutine when the object is initialized
+        StartCoroutine(CountUpRoutine());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator CountUpRoutine()
     {
-        
+        while (true)
+        {
+            // Wait for 1 second in real-time before continuing
+            yield return new WaitForSeconds(1f);
+            
+            counter++;
+            Debug.Log("Counter: " + counter);
+        }
     }
+     */
 }
